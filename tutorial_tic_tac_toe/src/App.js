1b5a54eb65ba4c944ada5fe1bdac33acb7bcab6f
abarreaux <alexis.barreaux@greenly.earth>
2025-06-05 16:36:37 +0200

    feat: add green color for winner

--- tutorial_tic_tac_toe/src/App.js
+++ tutorial_tic_tac_toe/src/App.js
@@ -50,17 +50,16 @@ export default function Game() {
 
 function Board({ xIsNext, squares, onPlay, currentMove }) {
   function handleClick(i) {
-    if (squares[i] !== null || calculateWinner(squares)) {
+    if (squares[i] !== null || calculateWinner(squares).winner) {
       return;
     }
-
     const nextSquares = squares.slice();
     nextSquares[i] = xIsNext ? "X" : "O";
     
     onPlay(nextSquares);
   }
 
-  const winner = calculateWinner(squares);
+  const {winner, winningLine} = calculateWinner(squares);
   let status;
   if (winner) {
     status = "Winner: " + winner;
@@ -77,9 +76,10 @@ function Board({ xIsNext, squares, onPlay, currentMove }) {
     const squareComponents = [];
     for(let j=0; j < 3; j++) {
       const squareIndex = i * 3 + j;
-      squareComponents.push(<Square value={squares[squareIndex]} onSquareClick={() => handleClick(squareIndex)}/>);
+      const isWinningIndex = winningLine && winningLine.includes(squareIndex);
+      squareComponents.push(<Square key={squareIndex} value={squares[squareIndex]} onSquareClick={() => handleClick(squareIndex)} isWinningIndex={isWinningIndex}/>);
     }
-    const rowDiv = <div className="board-row">{squareComponents}</div>;
+    const rowDiv = <div className="board-row" key={i}>{squareComponents}</div>;
     rowDivs.push(rowDiv);
   }
 
@@ -90,8 +90,8 @@ function Board({ xIsNext, squares, onPlay, currentMove }) {
   </> );
 }
 
-function Square({value, onSquareClick}) {
-  return <button className="square" onClick={onSquareClick}>{value}</button>;
+function Square({value, onSquareClick, isWinningIndex}) {
+  return <button className={"square" + (isWinningIndex ? " winner" : "") } onClick={onSquareClick}>{value}</button>;
 }
 
 function calculateWinner(squares) {
@@ -108,8 +108,8 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return {winner: squares[a], winningLine: lines[i]};
     }
   }
-  return null;
+  return {winner: null, winningLine: null};
 }
